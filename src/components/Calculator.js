5fb8b943d67464f868a34ef56eceb9b429c40b87
Webmaeistro <marander@protonmail.com>
2020-10-04 01:31:06 +0200

    init commit

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,19 +8,19 @@ export const Calculator = () => {
 	const [depositAmount, setDepositAmount] = useState(72000);
 	const [purchasingHousePrice, setPurchasingHousePrice] = useState(285000);
 	const [amountToBorrow, setAmountToBorrow] = useState(213000);
-	const [houslonTerm, setHouslonTerm] = useState(25);
-	const [interestRate, setInterestRate] = useState(1.8);
+	const [houseloanTerm, setHouseloanTerm] = useState(10);
+	const [interestRate, setInterestRate] = useState(2.1);
 	const [monthlyPayment, setMonthlyPayment] = useState(882);
 	const [totalRepaid, setTotalRepaid] = useState(264663);
 	const [totalInterestPaid, setTotalInterestPaid] = useState(51663);
 	const [yearlyPayments, setYearlyPayments] = useState([{"year":0,"outstandingBalance":213000,"interestPaid":0,"interestPaidToDate":0,"principalRepaid":0,"principalRepaidToDate":0},{"year":1,"outstandingBalance":206191.44,"interestPaid":3778.01,"interestPaidToDate":3778.01,"principalRepaid":6808.56,"principalRepaidToDate":6808.56},{"year":2,"outstandingBalance":199259.31,"interestPaid":3654.44,"interestPaidToDate":7432.45,"principalRepaid":6932.13,"principalRepaidToDate":13740.69},{"year":3,"outstandingBalance":192201.36,"interestPaid":3528.63,"interestPaidToDate":10961.08,"principalRepaid":7057.95,"principalRepaidToDate":20798.64},{"year":4,"outstandingBalance":185015.32,"interestPaid":3400.53,"interestPaidToDate":14361.61,"principalRepaid":7186.04,"principalRepaidToDate":27984.68},{"year":5,"outstandingBalance":177698.86,"interestPaid":3270.11,"interestPaidToDate":17631.72,"principalRepaid":7316.46,"principalRepaidToDate":35301.14},{"year":6,"outstandingBalance":170249.6,"interestPaid":3137.32,"interestPaidToDate":20769.05,"principalRepaid":7449.25,"principalRepaidToDate":42750.4},{"year":7,"outstandingBalance":162665.16,"interestPaid":3002.12,"interestPaidToDate":23771.17,"principalRepaid":7584.45,"principalRepaidToDate":50334.84},{"year":8,"outstandingBalance":154943.05,"interestPaid":2864.47,"interestPaidToDate":26635.64,"principalRepaid":7722.1,"principalRepaidToDate":58056.95},{"year":9,"outstandingBalance":147080.8,"interestPaid":2724.32,"interestPaidToDate":29359.97,"principalRepaid":7862.25,"principalRepaidToDate":65919.2},{"year":10,"outstandingBalance":139075.86,"interestPaid":2581.63,"interestPaidToDate":31941.59,"principalRepaid":8004.95,"principalRepaidToDate":73924.14},{"year":11,"outstandingBalance":130925.63,"interestPaid":2436.34,"interestPaidToDate":34377.94,"principalRepaid":8150.23,"principalRepaidToDate":82074.37},{"year":12,"outstandingBalance":122627.48,"interestPaid":2288.42,"interestPaidToDate":36666.36,"principalRepaid":8298.15,"principalRepaidToDate":90372.52},{"year":13,"outstandingBalance":114178.72,"interestPaid":2137.82,"interestPaidToDate":38804.18,"principalRepaid":8448.76,"principalRepaidToDate":98821.28},{"year":14,"outstandingBalance":105576.63,"interestPaid":1984.48,"interestPaidToDate":40788.66,"principalRepaid":8602.09,"principalRepaidToDate":107423.37},{"year":15,"outstandingBalance":96818.41,"interestPaid":1828.36,"interestPaidToDate":42617.02,"principalRepaid":8758.22,"principalRepaidToDate":116181.59},{"year":16,"outstandingBalance":87901.24,"interestPaid":1669.4,"interestPaidToDate":44286.42,"principalRepaid":8917.17,"principalRepaidToDate":125098.76},{"year":17,"outstandingBalance":78822.23,"interestPaid":1507.56,"interestPaidToDate":45793.99,"principalRepaid":9079.01,"principalRepaidToDate":134177.77},{"year":18,"outstandingBalance":69578.45,"interestPaid":1342.79,"interestPaidToDate":47136.77,"principalRepaid":9243.79,"principalRepaidToDate":143421.55},{"year":19,"outstandingBalance":60166.89,"interestPaid":1175.02,"interestPaidToDate":48311.79,"principalRepaid":9411.56,"principalRepaidToDate":152833.11},{"year":20,"outstandingBalance":50584.52,"interestPaid":1004.21,"interestPaidToDate":49316,"principalRepaid":9582.37,"principalRepaidToDate":162415.48},{"year":21,"outstandingBalance":40828.24,"interestPaid":830.29,"interestPaidToDate":50146.29,"principalRepaid":9756.28,"principalRepaidToDate":172171.76},{"year":22,"outstandingBalance":30894.89,"interestPaid":653.22,"interestPaidToDate":50799.52,"principalRepaid":9933.35,"principalRepaidToDate":182105.11},{"year":23,"outstandingBalance":20781.26,"interestPaid":472.94,"interestPaidToDate":51272.46,"principalRepaid":10113.63,"principalRepaidToDate":192218.74},{"year":24,"outstandingBalance":10484.07,"interestPaid":289.39,"interestPaidToDate":51561.85,"principalRepaid":10297.19,"principalRepaidToDate":202515.93},{"year":25,"outstandingBalance":0,"interestPaid":102.5,"interestPaidToDate":51664.35,"principalRepaid":10484.07,"principalRepaidToDate":213000}]);
 
-	const handleHouslonDataChange = (depositAmount, purchasingHousePrice, houslonTerm, interestRate) => {
+	const handleHouseloanDataChange = (depositAmount, purchasingHousePrice, houseloanTerm, interestRate) => {
 
-		//Set initial values for the whole houslon term
+		//Set initial values for the whole houseloan term
 		const amountToBorrow = purchasingHousePrice - depositAmount;
-		const monthlyPayment = ((interestRate / 100 / 12)*amountToBorrow) / (1 - (Math.pow(( 1+ (interestRate / 100 / 12)),((0 - houslonTerm) * 12))));
-		const totalRepaid = monthlyPayment * 12 * houslonTerm;
+		const monthlyPayment = ((interestRate / 100 / 12)*amountToBorrow) / (1 - (Math.pow(( 1+ (interestRate / 100 / 12)),((0 - houseloanTerm) * 12))));
+		const totalRepaid = monthlyPayment * 12 * houseloanTerm;
 		const totalInterestPaid = totalRepaid - amountToBorrow;
 
 		//Set initial values for loop to calculate yearly figures
@@ -36,8 +36,8 @@ export const Calculator = () => {
 		let interestPaidToDate = 0;
 		let principalRepaidToDate = 0;
 
-		//Loop each year of the houslon term
-		for(let i = 1; i <= houslonTerm; i++) {
+		//Loop each year of the houseloan term
+		for(let i = 1; i <= houseloanTerm; i++) {
 
 			let monthInterestPaid = 0;
 			let interestPaidMonthlyToYearlyIncrementer = 0;
@@ -56,8 +56,8 @@ export const Calculator = () => {
 			interestPaidToDate = interestPaidToDate + interestPaidMonthlyToYearlyIncrementer;
 			principalRepaidToDate = principalRepaidToDate + monthlyPrincipalRepaidToYearlyIncrementer;
 
-			//There's always around £10 left at the end which forces the fraph to go into minus. This just rounds the last figure off at £0.00.
-			if(i === houslonTerm) {
+			//There's always around kr10 left at the end which forces the fraph to go into minus. This just rounds the last figure off at kr0.00.
+			if(i === houseloanTerm) {
 				outstandingBalance = 0;
 			}
 
@@ -74,7 +74,7 @@ export const Calculator = () => {
 		setDepositAmount(depositAmount);
 		setPurchasingHousePrice(purchasingHousePrice);
 		setAmountToBorrow(amountToBorrow);
-		setHouslonTerm(houslonTerm);
+		setHouseloanTerm(houseloanTerm);
 		setInterestRate(interestRate);
 		setMonthlyPayment(monthlyPayment);
 		setTotalRepaid(totalRepaid);
@@ -84,8 +84,8 @@ export const Calculator = () => {
 	}
 
 	useEffect(() => {
-		handleHouslonDataChange(depositAmount, purchasingHousePrice, houslonTerm, interestRate);
-	},[depositAmount, purchasingHousePrice, houslonTerm, interestRate]);
+		handleHouseloanDataChange(depositAmount, purchasingHousePrice, houseloanTerm, interestRate);
+	},[depositAmount, purchasingHousePrice, houseloanTerm, interestRate]);
 
 	return(
 		<div>
@@ -96,14 +96,14 @@ export const Calculator = () => {
 				totalInterestPaid={totalInterestPaid}
 			/>
 			<CalculatorControls 
-				handleHouslonDataChange={handleHouslonDataChange} 
+				handleHouseloanDataChange={handleHouseloanDataChange} 
 				depositAmount={depositAmount}
 				purchasingHousePrice={purchasingHousePrice}
-				houslonTerm={houslonTerm}
+				houseloanTerm={houseloanTerm}
 				interestRate={interestRate}
 			/>
 			<PaymentGraph
-				houslonTerm={houslonTerm}
+				houseloanTerm={houseloanTerm}
 				yearlyPayments={yearlyPayments}
 			/>
 			<PaymentTable
--- src/components/CalculatorControls.js
+++ src/components/CalculatorControls.js
@@ -17,13 +17,13 @@ export const CalculatorControls = props => {
 					className="grid__item--range-slider"
 					id="purchasingHousePrice"  
 					min="50000" 
-					max="750000" 
+					max="7500000" 
 					step="1000" 
-					defaultValue="285000" 
-					onChange={() => props.handleHouslonDataChange(
+					defaultValue="274000" 
+					onChange={() => props.handleHouseloanDataChange(
 						parseFloat(document.getElementById('depositAmount').value), 
 						parseFloat(document.getElementById('purchasingHousePrice').value), 
-						parseFloat(document.getElementById('houslonTerm').value), 
+						parseFloat(document.getElementById('houseloanTerm').value), 
 						parseFloat(document.getElementById('interestRate').value))} />
 				<label className="grid__item--label" htmlFor="purchasingHousePrice">Purchasing House Price</label>
 			</div>
@@ -34,32 +34,32 @@ export const CalculatorControls = props => {
 					className="grid__item--range-slider"
 					id="depositAmount" 
 					min="1000" 
-					max="150000" 
+					max="1000000" 
 					step="1000" 
-					defaultValue="72000" 
-					onChange={() => props.handleHouslonDataChange(
+					defaultValue="42000" 
+					onChange={() => props.handleHouseloanDataChange(
 						parseFloat(document.getElementById('depositAmount').value), 
 						parseFloat(document.getElementById('purchasingHousePrice').value), 
-						parseFloat(document.getElementById('houslonTerm').value), 
+						parseFloat(document.getElementById('houseloanTerm').value), 
 						parseFloat(document.getElementById('interestRate').value))} />
-				<label className="grid__item--label" htmlFor="points">Deposit</label>
+				<label className="grid__item--label" htmlFor="points">Own Capital</label>
 			</div>
 			<div className="grid__item">
-				<span className="grid__item--header">{props.houslonTerm} Years</span>
+				<span className="grid__item--header">{props.houseloanTerm} Years</span>
 				<input 
 					type="range"
 					className="grid__item--range-slider"
-					id="houslonTerm"  
+					id="houseloanTerm"  
 					min="5" 
 					max="35" 
 					step="1" 
-					defaultValue="25" 
-					onChange={() => props.handleHouslonDataChange(
+					defaultValue="15" 
+					onChange={() => props.handleHouseloanDataChange(
 						parseFloat(document.getElementById('depositAmount').value), 
 						parseFloat(document.getElementById('purchasingHousePrice').value), 
-						parseFloat(document.getElementById('houslonTerm').value), 
+						parseFloat(document.getElementById('houseloanTerm').value), 
 						parseFloat(document.getElementById('interestRate').value))} />
-				<label className="grid__item--label" htmlFor="houslonTerm">Houslon Term</label>
+				<label className="grid__item--label" htmlFor="houseloanTerm">Loan Periode/Term</label>
 			</div>
 			<div className="grid__item">
 				<span className="grid__item--header">{props.interestRate}%</span>
@@ -68,13 +68,13 @@ export const CalculatorControls = props => {
 					className="grid__item--range-slider"
 					id="interestRate"  
 					min="0.1" 
-					max="8" 
-					step="0.1" 
-					defaultValue="1.8" 
-					onChange={() => props.handleHouslonDataChange(
+					max="12" 
+					step="0.25" 
+					defaultValue="0.8" 
+					onChange={() => props.handleHouseloanDataChange(
 						parseFloat(document.getElementById('depositAmount').value), 
 						parseFloat(document.getElementById('purchasingHousePrice').value), 
-						parseFloat(document.getElementById('houslonTerm').value), 
+						parseFloat(document.getElementById('houseloanTerm').value), 
 						parseFloat(document.getElementById('interestRate').value))} />
 				<label className="grid__item--label" htmlFor="interestRate">Interest Rate</label>
 			</div>
